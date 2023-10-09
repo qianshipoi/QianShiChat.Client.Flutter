5273953d1766c349a4580f57a8c4c4490cf27e71
qianshi <2410128587@qq.com>
2023-10-09 20:32:13 +0800

    init

---
+++ lib/main.dart
@@ -0,0 +1,77 @@
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:qianshi_chat/models/userinfo.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return ChangeNotifierProvider(
+      create: (context) => MyAppState(),
+      builder: (context, _) {
+        return MaterialApp(
+          title: 'Namer App',
+          theme: ThemeData(
+            colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+            useMaterial3: true,
+          ),
+          home: MyHomePage(),
+        );
+      },
+    );
+  }
+}
+
+class MyAppState extends ChangeNotifier {
+  UserInfo? _userInfo;
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key});
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  int _counter = 0;
+
+  void _incrementCounter() {
+    setState(() {
+      _counter++;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            const Text(
+              'You have pushed the button this many times:',
+            ),
+            Text(
+              '$_counter',
+              style: Theme.of(context).textTheme.headlineMedium,
+            ),
+          ],
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: _incrementCounter,
+        tooltip: 'Increment',
+        child: const Icon(Icons.add),
+      ),
+    );
+  }
+}
---
+++ lib/models/userinfo.dart
@@ -0,0 +1,99 @@
+// ignore_for_file: public_member_api_docs, sort_constructors_first
+import 'dart:convert';
+
+class UserInfo {
+  int id;
+  String account;
+  String nickName;
+  String avatar;
+  int createTime;
+  bool isOnline;
+  String alias;
+  UserInfo({
+    required this.id,
+    required this.account,
+    required this.nickName,
+    required this.avatar,
+    required this.createTime,
+    required this.isOnline,
+    required this.alias,
+  });
+
+  UserInfo copyWith({
+    int? id,
+    String? account,
+    String? nickName,
+    String? avatar,
+    int? createTime,
+    bool? isOnline,
+    String? alias,
+  }) {
+    return UserInfo(
+      id: id ?? this.id,
+      account: account ?? this.account,
+      nickName: nickName ?? this.nickName,
+      avatar: avatar ?? this.avatar,
+      createTime: createTime ?? this.createTime,
+      isOnline: isOnline ?? this.isOnline,
+      alias: alias ?? this.alias,
+    );
+  }
+
+  Map<String, dynamic> toMap() {
+    return <String, dynamic>{
+      'id': id,
+      'account': account,
+      'nickName': nickName,
+      'avatar': avatar,
+      'createTime': createTime,
+      'isOnline': isOnline,
+      'alias': alias,
+    };
+  }
+
+  factory UserInfo.fromMap(Map<String, dynamic> map) {
+    return UserInfo(
+      id: map['id'] as int,
+      account: map['account'] as String,
+      nickName: map['nickName'] as String,
+      avatar: map['avatar'] as String,
+      createTime: map['createTime'] as int,
+      isOnline: map['isOnline'] as bool,
+      alias: map['alias'] as String,
+    );
+  }
+
+  String toJson() => json.encode(toMap());
+
+  factory UserInfo.fromJson(String source) => UserInfo.fromMap(json.decode(source) as Map<String, dynamic>);
+
+  @override
+  String toString() {
+    return 'UserInfo(id: $id, account: $account, nickName: $nickName, avatar: $avatar, createTime: $createTime, isOnline: $isOnline, alias: $alias)';
+  }
+
+  @override
+  bool operator ==(covariant UserInfo other) {
+    if (identical(this, other)) return true;
+
+    return
+      other.id == id &&
+      other.account == account &&
+      other.nickName == nickName &&
+      other.avatar == avatar &&
+      other.createTime == createTime &&
+      other.isOnline == isOnline &&
+      other.alias == alias;
+  }
+
+  @override
+  int get hashCode {
+    return id.hashCode ^
+      account.hashCode ^
+      nickName.hashCode ^
+      avatar.hashCode ^
+      createTime.hashCode ^
+      isOnline.hashCode ^
+      alias.hashCode;
+  }
+}
