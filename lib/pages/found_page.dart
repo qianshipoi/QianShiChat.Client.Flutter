ee44dbf86155f898ba9acb6acdee6e0e1cc79b4b
未来 栗山 <2410128587@qq.com>
2023-10-11 22:35:23 +0800

    feat: tabs

--- lib/pages/found_page.dart
+++ lib/pages/found_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:qianshi_chat/utils/common_sliver_header_delegate.dart';
 
 class FoundPage extends StatefulWidget {
   const FoundPage({super.key});
@@ -7,11 +8,101 @@ class FoundPage extends StatefulWidget {
   State<StatefulWidget> createState() => _FoundPageState();
 }
 
-class _FoundPageState extends State<FoundPage> {
+class _FoundPageState extends State<FoundPage>
+    with SingleTickerProviderStateMixin {
+  late TabController _tabController;
+
+  @override
+  void initState() {
+    super.initState();
+    _tabController = TabController(
+        vsync: this, // 动画效果的异步处理
+        length: 3 // tab 个数
+        );
+  }
+
+  @override
+  void dispose() {
+    _tabController.dispose();
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
-    return const Center(
-      child: Text('FoundPage'),
+    return SafeArea(
+      top: true,
+      child: NestedScrollView(
+        headerSliverBuilder: (BuildContext context, bool innerBoxIsScrolled) {
+          return <Widget>[
+            //写类似于SliverAppBar中的title部分。可以自定义图标，标题，内容
+            SliverPersistentHeader(
+                delegate: CommonSliverHeaderDelegate(
+                    islucency: true,
+                    child: PreferredSize(
+                        preferredSize: const Size.fromHeight(60),
+                        child: Row(
+                          children: [
+                            Container(
+                              padding: const EdgeInsets.all(20),
+                              alignment: Alignment.center,
+                              child: const Text('sdfsdf'),
+                            ),
+                            Container(
+                              padding: const EdgeInsets.all(20),
+                              alignment: Alignment.center,
+                              child: const Text('sdfsdf'),
+                            ),
+                            Container(
+                              padding: const EdgeInsets.all(20),
+                              alignment: Alignment.center,
+                              child: const Icon(Icons.add),
+                            ),
+                          ],
+                        )))), //tabbar悬停
+            SliverPersistentHeader(
+                floating: true,
+                pinned: true,
+                delegate: CommonSliverHeaderDelegate(
+                    islucency: false,
+                    child: PreferredSize(
+                        preferredSize: const Size(double.infinity, 30),
+                        child: TabBar(
+                          controller: _tabController,
+                          indicatorSize: TabBarIndicatorSize.label,
+                          tabs: const <Widget>[
+                            Tab(
+                              child: Text('测试'),
+                            ),
+                            Tab(
+                              child: Text('测试'),
+                            ),
+                            Tab(
+                              child: Text('测试'),
+                            ),
+                          ],
+                        )))),
+
+            // SliverGrid(
+            //   gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+            //       crossAxisCount: 3, crossAxisSpacing: 5, mainAxisSpacing: 3),
+            //   delegate:
+            //       SliverChildBuilderDelegate((BuildContext context, int index) {
+            //     return Container(
+            //       color: Colors.primaries[index % Colors.primaries.length],
+            //     );
+            //   }, childCount: 20),
+            // )
+          ];
+        },
+        body: TabBarView(
+          controller: _tabController, //tabbar控制器
+          children: const <Widget>[
+            Text('sdf'),
+            Text('sdf'),
+            Text('sdf'),
+          ],
+        ),
+      ),
     );
   }
 }
